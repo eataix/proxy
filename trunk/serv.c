8d6f1f9f42e6af7ba342a0295daf5f4b4b132058
u4700480 <u4700480@da65007c-86a0-4cae-9f44-452e394fb7d8>
2012-05-15 15:16:36 +0000

    Use signal() and fork().
    git-svn-id: https://svn.anu.edu.au/Networks/A2Grp39@8 da65007c-86a0-4cae-9f44-452e394fb7d8

--- trunk/serv.c
+++ trunk/serv.c
@@ -32,150 +32,180 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "dbg.h"
 #include "readline.h"
 
 #define BUFFER_SIZE INT16_MAX
 
-        void
+void
+sigHandler(int sig)
+{
+    if (sig == SIGINT) {
+        printf("Catch interrupt.\n");
+        kill(0, SIGTERM);
+    } else if (sig == SIGTERM) {
+        sleep(2);
+        printf("Catch termination.\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void
+childSigHandler(int sig)
+{
+    if (sig == SIGTERM) {
+        printf("%d is going to terminate\n", getpid());
+        _exit(0);
+    }
+}
+
+void
 copybytes(char *s1, const char *s2, const int num)
 {
-        char           *p1 = s1;
-        const char     *p2 = s2;
-        while (*p2 != '\0') {
-                memcpy(s1, s2, 1);
-        }
-        return;
+    char           *p1 = s1;
+    const char     *p2 = s2;
+    while (*p2 != '\0') {
+        memcpy(s1, s2, 1);
+    }
+    return;
 }
-        int
+int
 main(int argc, char *argv[])
 {
-        int             sfd,
-                        newfd,
-                        newfd2;
-        struct addrinfo hints,
-                        *servinfo;
-        struct sockaddr_in their_addr;
-        socklen_t       addr_size;
-        char           *buffer;
-        int             byte_count;
-        char            reply[100];
-        int             index = 0;
-        char           *host = NULL;
-        int             optval;
-        int             optlen;
-        char           *optval2;
+    int             sfd,
+                    newfd,
+                    newfd2;
+    struct addrinfo hints,
+                   *servinfo;
+    struct sockaddr_in their_addr;
+    socklen_t       addr_size;
+    char           *buffer;
+    int             byte_count;
+    char            reply[100];
+    int             index = 0;
+    char           *host = NULL;
+    int             optval;
+    int             optlen;
+    char           *optval2;
 
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGINT, sigHandler);
+    signal(SIGTERM, sigHandler);
 
-        check(getaddrinfo(NULL, "3310", &hints, &servinfo) == 0,
-                        "cannot getaddrinfo");
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-        sfd =
-                socket(servinfo->ai_family, servinfo->ai_socktype,
-                                servinfo->ai_protocol);
+    check(getaddrinfo(NULL, "3310", &hints, &servinfo) == 0,
+          "cannot getaddrinfo");
 
-        optval = 1;
-        setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    sfd =
+        socket(servinfo->ai_family, servinfo->ai_socktype,
+               servinfo->ai_protocol);
 
-        check(bind(sfd, servinfo->ai_addr, servinfo->ai_addrlen) != -1,
-                        "cannot bind.");
+    optval = 1;
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-        check(listen(sfd, 10) != -1, "Cannot listen");
+    check(bind(sfd, servinfo->ai_addr, servinfo->ai_addrlen) != -1,
+          "cannot bind.");
 
-
-        host = malloc(100);
-
-        while (1) {
-                memset(host, 0, strlen(host));
-                addr_size = sizeof(their_addr);
-                check((newfd =
-                                        accept(sfd, (struct sockaddr *) &their_addr,
-                                                &addr_size)) != -1, "cannot accept");
-
-                if (fork() == 0) {
-                        buffer = malloc(BUFFER_SIZE);
-                        byte_count = recv(newfd, buffer, BUFFER_SIZE - 1, 0);
-                        buffer[byte_count] = '\0';
-
-                        printf("%s", buffer);
-
-                        if ((host = strcasestr(buffer, "Host: ")) == NULL) {
-                                return EXIT_FAILURE;
-                        }
-                        //printf("Host is: %d", (host - buffer));
-
-                        int             length = strcspn(host, "\r\n") - 6;
-                        char           *hostname = malloc(length + 1);
-                        *hostname = '\0';
-                        strncat(hostname, host + 6, length);
-
-                        printf("Will now connect to %s\n", hostname);
-
-                        memset(&hints, 0, sizeof(hints));
-                        hints.ai_family = AF_INET;
-                        hints.ai_socktype = SOCK_STREAM;
+    check(listen(sfd, 10) != -1, "Cannot listen");
 
 
-                        struct addrinfo *server;
-                        if (getaddrinfo(hostname, "http", &hints, &server) == 0) {
-                                //printf("resolved\n");
-                        }
+    host = malloc(100);
 
-                        int             serversocket =
-                                socket(server->ai_family, server->ai_socktype,
-                                                server->ai_protocol);
+    while (1) {
+        memset(host, 0, strlen(host));
+        addr_size = sizeof(their_addr);
+        check((newfd =
+               accept(sfd, (struct sockaddr *) &their_addr,
+                      &addr_size)) != -1, "cannot accept");
 
-                        if (serversocket != -1) {
-                                //printf("Created a new file descriptor\n");
-                        }
+        if (fork() == 0) {
+            signal(SIGTERM, childSigHandler);
+            buffer = malloc(BUFFER_SIZE);
+            byte_count = recv(newfd, buffer, BUFFER_SIZE - 1, 0);
+            buffer[byte_count] = '\0';
 
-                        if (connect(serversocket, server->ai_addr, server->ai_addrlen)
-                                        == 0) {
-                                //printf("Connected.\n");
-                        }
+            printf("%s", buffer);
 
-                        send(serversocket, buffer, byte_count, 0);
+            if ((host = strcasestr(buffer, "Host: ")) == NULL) {
+                return EXIT_FAILURE;
+            }
+            // printf("Host is: %d", (host - buffer));
 
-                        struct timeval tv;
-                        fd_set readfds;
+            int             length = strcspn(host, "\r\n") - 6;
+            char           *hostname = malloc(length + 1);
+            *hostname = '\0';
+            strncat(hostname, host + 6, length);
 
-                        tv.tv_sec = 1;
-                        tv.tv_usec = 0;
-                        int total_bytes = 0;
+            printf("Will now connect to %s\n", hostname);
 
-                        while (1) {
-                                FD_ZERO(&readfds);
-                                FD_SET(serversocket, &readfds);
-                                select(serversocket + 1, &readfds, NULL, NULL, &tv);
+            memset(&hints, 0, sizeof(hints));
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_STREAM;
 
-                                if (FD_ISSET(serversocket, &readfds)) {
-                                        byte_count = recv(serversocket, buffer + total_bytes, BUFFER_SIZE, 0);
-                                        if (byte_count == 0)
-                                                break;
-                                        else if (byte_count == -1)
-                                                break;
-                                        total_bytes += byte_count;
-                                } else {
-                                        break;
-                                }
-                        }
 
-                        //printf("About to send.\n");
-                        send(newfd, buffer, total_bytes, 0);
-                        close(newfd);
-                        exit(0);
+            struct addrinfo *server;
+            if (getaddrinfo(hostname, "http", &hints, &server) == 0) {
+                // printf("resolved\n");
+            }
+
+            int             serversocket =
+                socket(server->ai_family, server->ai_socktype,
+                       server->ai_protocol);
+
+            if (serversocket != -1) {
+                // printf("Created a new file descriptor\n");
+            }
+
+            if (connect(serversocket, server->ai_addr, server->ai_addrlen)
+                == 0) {
+                // printf("Connected.\n");
+            }
+
+            send(serversocket, buffer, byte_count, 0);
+
+            struct timeval  tv;
+            fd_set          readfds;
+
+            tv.tv_sec = 1;
+            tv.tv_usec = 0;
+            int             total_bytes = 0;
+
+            while (1) {
+                FD_ZERO(&readfds);
+                FD_SET(serversocket, &readfds);
+                select(serversocket + 1, &readfds, NULL, NULL, &tv);
+
+                if (FD_ISSET(serversocket, &readfds)) {
+                    byte_count =
+                        recv(serversocket, buffer + total_bytes,
+                             BUFFER_SIZE, 0);
+                    if (byte_count == 0)
+                        break;
+                    else if (byte_count == -1)
+                        break;
+                    total_bytes += byte_count;
+                } else {
+                    break;
                 }
-                close(newfd);
-                //printf("finish");
+            }
+
+            // printf("About to send.\n");
+            send(newfd, buffer, total_bytes, 0);
+            close(newfd);
+            _exit(0);
         }
+        close(newfd);
+        // printf("finish");
+    }
 
-        return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 
-error:
-        return EXIT_FAILURE;
+  error:
+    return EXIT_FAILURE;
 }
