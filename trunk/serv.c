ef695e1aebeec6aca9cc0f13e6d8f7b535a4bddb
u4700480 <u4700480@da65007c-86a0-4cae-9f44-452e394fb7d8>
2012-05-15 15:16:44 +0000

    Fix a bug that will cause 404.
    git-svn-id: https://svn.anu.edu.au/Networks/A2Grp39@14 da65007c-86a0-4cae-9f44-452e394fb7d8

--- trunk/serv.c
+++ trunk/serv.c
@@ -42,324 +42,349 @@
 #define BUFFER_SIZE INT16_MAX
 
 struct peer {
-        int             socketfd;
-        int             flag;
-        char           *buffer;
-        int             bytes_read;
+    int             socketfd;
+    int             flag;
+    char           *buffer;
+    int             bytes_read;
 };
 
-        void
+void
 sigHandler(int sig)
 {
-        if (sig == SIGINT) {
-                printf("Catch interrupt.\n");
-                kill(0, SIGTERM);
-        } else if (sig == SIGTERM) {
-                sleep(2);
-                printf("Catch termination.\n");
-                exit(EXIT_SUCCESS);
-        }
+    if (sig == SIGINT) {
+        printf("Catch interrupt.\n");
+        kill(0, SIGTERM);
+    } else if (sig == SIGTERM) {
+        sleep(2);
+        printf("Catch termination.\n");
+        exit(EXIT_SUCCESS);
+    }
 }
 
-        void
+void
 childSigHandler(int sig)
 {
-        if (sig == SIGTERM) {
-                printf("%d is going to terminate\n", getpid());
-                _exit(0);
-        }
-}
-
-        int
-make_socket(const char *name, const char *port)
-{
-        struct addrinfo hints,
-                        *ai,
-                        *p;
-        int             s;
-        printf("prepare to connect to %s\n", name);
-
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_UNSPEC;
-        hints.ai_socktype = SOCK_STREAM;
-        if (name == NULL) {
-                hints.ai_flags = AI_PASSIVE;
-        }
-
-        if (getaddrinfo(name, port, &hints, &ai) != 0) {
-                printf("Cannot getaddrinfo");
-                _exit(1);
-        }
-
-        for (p = ai; p != NULL; p = p->ai_next) {
-                s = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-                if (s < 0)
-                        continue;
-                printf("created a file discriptor\n");
-                if (connect(s, p->ai_addr, p->ai_addrlen) != 0)
-                        continue;
-                printf("Connected\n");
-                return s;
-        }
-        return -1;
-}
-
-        char           *
-extract_header(const char *line, const char *key)
-{
-        int             length;
-        char           *header = NULL;
-        int             prefixLength;
-
-        prefixLength = strlen(key);
-
-        length = strcspn(line, "\r\n") - prefixLength;
-
-        header = malloc(length + 1);
-        check(header != NULL, "Cannot allocate memory to extra");
-        *header = '\0';
-        strncat(header, line + prefixLength, length);
-        return header;
-
-error:
-        return NULL;
+    if (sig == SIGTERM) {
+        printf("%d is going to terminate\n", getpid());
+        _exit(0);
+    }
 }
 
 int
-process(char *str, const int str_len) {
+make_socket(const char *name, const char *port)
+{
+    struct addrinfo hints,
+                   *ai,
+                   *p;
+    int             s;
+    printf("prepare to connect to %s\n", name);
 
-        const char *prefix = "http://";
-        int length = strlen(prefix);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if (name == NULL) {
+        hints.ai_flags = AI_PASSIVE;
+    }
 
-        char *p = malloc(str_len + 1);
-        memset(p, 0, sizeof(*p));
-        memcpy(p, str, str_len);
+    if (getaddrinfo(name, port, &hints, &ai) != 0) {
+        printf("Cannot getaddrinfo");
+        _exit(1);
+    }
 
-
-        int i, j;
-        for (i = 0, j = 0; i < str_len && j < str_len; i++, j++) {
-                if (strncasecmp(p + j, prefix, length - 1) == 0) {
-                        for (j += length; ;j++) {
-                                if (p[j] == '/'){
-                                        break;
-                                }
-                                if (p[j] == ' ') {
-                                        str[i++] = '/';
-                                        break;
-                                }
-                        }
-                }
-                str[i] = p[j];
-        }
-
-        return i;
+    for (p = ai; p != NULL; p = p->ai_next) {
+        s = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (s < 0)
+            continue;
+        printf("created a file discriptor\n");
+        if (connect(s, p->ai_addr, p->ai_addrlen) != 0)
+            continue;
+        printf("Connected\n");
+        return s;
+    }
+    return -1;
 }
 
-        void
+char           *
+extract_header(const char *line, const char *key)
+{
+    int             length;
+    char           *header = NULL;
+    int             prefixLength;
+
+    prefixLength = strlen(key);
+
+    length = strcspn(line, "\r\n") - prefixLength;
+
+    header = malloc(length + 1);
+    check(header != NULL, "Cannot allocate memory to extra");
+    *header = '\0';
+    strncat(header, line + prefixLength, length);
+    return header;
+
+  error:
+    return NULL;
+}
+
+int
+process(char *str, const int str_len)
+{
+
+    const char     *prefix = "http://";
+    int             length = strlen(prefix);
+
+    char           *p = malloc(str_len + 1);
+    memset(p, 0, sizeof(*p));
+    memcpy(p, str, str_len);
+
+
+    int             i,
+                    j;
+    for (i = 0, j = 0; i < str_len && j < str_len; i++, j++) {
+        if (strncasecmp(p + j, prefix, length - 1) == 0) {
+            for (j += length;; j++) {
+                if (p[j] == '/') {
+                    break;
+                }
+                if (p[j] == ' ') {
+                    str[i++] = '/';
+                    break;
+                }
+            }
+        }
+        str[i] = p[j];
+    }
+
+    return i;
+}
+
+void
 proxy(int sfd)
 {
-        int             byte_count = 0;
-        char           *hostname = NULL;
+    int             byte_count = 0;
+    char           *hostname = NULL;
 
-        signal(SIGTERM, childSigHandler);
+    signal(SIGTERM, childSigHandler);
 
-        struct peer    *client = malloc(sizeof(*client));
-        struct peer    *serveri = malloc(sizeof(*serveri));
+    struct peer    *client = malloc(sizeof(*client));
+    struct peer    *serveri = malloc(sizeof(*serveri));
 
-        memset(client, 0, sizeof(*client));
-        memset(serveri, 0, sizeof(*serveri));
+    memset(client, 0, sizeof(*client));
+    memset(serveri, 0, sizeof(*serveri));
 
-        serveri->socketfd = -1;
+    serveri->socketfd = -1;
 
-        check((client->buffer = malloc(BUFFER_SIZE)) != NULL,
-                        "Cannot allocate memory for buffer");
-        check((serveri->buffer = malloc(BUFFER_SIZE)) != NULL,
-                        "Cannot allocate memory for buffer");
+    check((client->buffer = malloc(BUFFER_SIZE)) != NULL,
+          "Cannot allocate memory for buffer");
+    check((serveri->buffer = malloc(BUFFER_SIZE)) != NULL,
+          "Cannot allocate memory for buffer");
 
-        fd_set          master,
-                        read_fds,
-                        write_fds;
+    fd_set          master,
+                    read_fds;
 
-        int             fdmax;
-        int line_count = 0;
+    int             fdmax;
+    int             line_count = 0;
 
-        FD_ZERO(&master);
-        FD_ZERO(&read_fds);
-        FD_ZERO(&write_fds);
-
-        FD_SET(sfd, &master);
-        client->socketfd = sfd;
-        //fdmax = sfd;
+    client->socketfd = sfd;
+    // fdmax = sfd;
 
 
-        for (;;) {
-                byte_count = readLine(client->socketfd, client->buffer + client->bytes_read,
-                                500);
-                line_count++;
+    for (;;) {
+        byte_count =
+            readLine(client->socketfd, client->buffer + client->bytes_read,
+                     500);
+        line_count++;
 
-                if (line_count == 1) {
-                        byte_count = process(client->buffer + client->bytes_read, byte_count);
-                }
-
-                if (strncmp(client->buffer + client->bytes_read, "Host: ", 6) ==0) {
-                        printf("host");
-                        hostname = extract_header(client->buffer + client->bytes_read, "Host: ");
-                        serveri->socketfd = make_socket(hostname, "http");
-                        //fcntl(sfd, F_SETFL, O_NONBLOCK);
-                }
-                client->bytes_read += byte_count;
-                if (byte_count == 2) {
-                        break;
-                }
+        if (line_count == 1) {
+            byte_count =
+                process(client->buffer + client->bytes_read, byte_count);
         }
 
-        if (serveri->socketfd == -1) {
-                _exit(EXIT_FAILURE);
+        if (strncmp(client->buffer + client->bytes_read, "Host: ", 6) == 0) {
+            printf("host");
+            hostname =
+                extract_header(client->buffer + client->bytes_read,
+                               "Host: ");
+            serveri->socketfd = make_socket(hostname, "http");
+            // fcntl(sfd, F_SETFL, O_NONBLOCK);
+        }
+        client->bytes_read += byte_count;
+        if (byte_count == 2) {
+            break;
+        }
+    }
+
+    if (serveri->socketfd == -1) {
+        _exit(EXIT_FAILURE);
+    }
+
+    send(serveri->socketfd, client->buffer, client->bytes_read, 0);
+
+
+    struct timeval  tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 500000;
+
+    client->bytes_read = 0;
+
+    FD_ZERO(&master);
+    FD_SET(sfd, &master);
+    fdmax = client->socketfd;
+    for (;;) {
+        read_fds = master;
+
+        if (select(fdmax + 1, &read_fds, NULL, NULL, &tv) == -1) {
+            printf("cannot select \n");
+            _exit(1);
         }
 
-        send(serveri->socketfd, client->buffer, client->bytes_read, 0);
+        if (FD_ISSET(client->socketfd, &read_fds)) {
+            // printf("read line from client\n");
+            byte_count =
+                recv(client->socketfd, client->buffer + client->bytes_read,
+                     1024, 0);
 
-        fdmax =
-                serveri->socketfd >
-                client->socketfd ? serveri->socketfd : client->socketfd;
+            if (byte_count == -1) {
+                _exit(1);
+            }
 
-        struct timeval tv;
-        tv.tv_sec = 0;
-        tv.tv_usec = 5000000;
+            client->bytes_read += byte_count;
 
-        client->bytes_read = 0;
-        serveri->bytes_read = 0;
+            byte_count =
+                send(serveri->socketfd, client->buffer, client->bytes_read,
+                     0);
 
-        for (;;) {
-                read_fds = master;
+            if (byte_count == 0) {
+                _exit(1);
+            }
 
-                if (select(fdmax + 1, &read_fds, NULL, NULL, &tv) == -1) {
-                        printf("cannot select \n");
-                        _exit(1);
-                }
+            client->bytes_read = 0;
 
-                if (FD_ISSET(client->socketfd, &read_fds)) {
-                        //printf("read line from client\n");
-                        byte_count =
-                                recv(client->socketfd, client->buffer + client->bytes_read, 1024, 0);
-
-                        if (byte_count == -1) {
-                                _exit(1);
-                        }
-
-                        int i;
-                        for (i = 0; i < byte_count; i++) {
-                                putchar((client->buffer + client ->bytes_read)[i]);
-                        }
-
-                        client->bytes_read += byte_count;
-
-                        byte_count = send(serveri->socketfd, client->buffer, client->bytes_read, 0);
-
-                        if (byte_count == 0) {
-                                _exit(1);
-                        }
-
-                        client->bytes_read = 0;
-
-                        continue;
-                }
-
-                if (serveri->socketfd != -1 && FD_ISSET(serveri->socketfd, &read_fds)) {
-                        byte_count =
-                                recv(serveri->socketfd, serveri->buffer + serveri->bytes_read, 1024, 0);
-
-                        if (byte_count == -1) {
-                                _exit(1);
-                        }
-
-                        serveri->bytes_read+= byte_count;
-
-
-                        byte_count = send(client->socketfd, serveri->buffer, serveri->bytes_read, 0);
-
-                        if (byte_count <= 0) {
-                                _exit(1);
-                        }
-
-                        serveri->bytes_read = 0;
-
-                        continue;
-                }
-
-                if (serveri->bytes_read != 0)
-                        send(client->socketfd, serveri->buffer, serveri->bytes_read, 0);
-
-                if (client->bytes_read != 0)
-                        send(serveri->socketfd, client->buffer, client->bytes_read, 0);
-
-                printf("finish\n");
-                free(serveri->buffer);
-                free(client->buffer);
-                close(serveri->socketfd);
-                close(client->socketfd);
-                _exit(0);
-
-                //printf("select\n");
+            continue;
         }
 
-error:
-        close(serveri->socketfd);
-        close(client->socketfd);
-        _exit(1);
+        if (client->bytes_read != 0)
+            send(serveri->socketfd, client->buffer, client->bytes_read, 0);
+        break;
+
+        // printf("select\n");
+    }
+
+
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
+
+    serveri->bytes_read = 0;
+
+    FD_ZERO(&master);
+    FD_SET(serveri->socketfd, &master);
+    fdmax = serveri->socketfd;
+    for (;;) {
+        read_fds = master;
+
+        if (select(fdmax + 1, &read_fds, NULL, NULL, &tv) == -1) {
+            printf("cannot select \n");
+            _exit(1);
+        }
+
+        if (serveri->socketfd != -1
+            && FD_ISSET(serveri->socketfd, &read_fds)) {
+            tv.tv_sec = 1;
+            byte_count =
+                recv(serveri->socketfd,
+                     serveri->buffer + serveri->bytes_read, 1024, 0);
+
+            // printf("read: %d bytes", byte_count);
+
+            if (byte_count == -1) {
+                _exit(1);
+            }
+
+            serveri->bytes_read += byte_count;
+
+            byte_count =
+                send(client->socketfd, serveri->buffer,
+                     serveri->bytes_read, 0);
+
+            if (byte_count <= 0) {
+                _exit(1);
+            }
+
+            serveri->bytes_read = 0;
+
+            continue;
+        }
+
+        if (serveri->bytes_read != 0)
+            send(client->socketfd, serveri->buffer, serveri->bytes_read,
+                 0);
+        break;
+
+    }
+
+    printf("finish\n");
+    free(serveri->buffer);
+    free(client->buffer);
+    close(serveri->socketfd);
+    close(client->socketfd);
+    _exit(0);
+
+  error:
+    close(serveri->socketfd);
+    close(client->socketfd);
+    _exit(1);
 }
 
-        int
+int
 main()
 {
-        int             sfd,
-                        newfd;
-        struct addrinfo hints,
-                        *servinfo;
-        int             optval;
-        //setbuf(stdout, NULL);
+    int             sfd,
+                    newfd;
+    struct addrinfo hints,
+                   *servinfo;
+    int             optval;
+    // setbuf(stdout, NULL);
 
-        signal(SIGCHLD, SIG_IGN);
-        signal(SIGINT, sigHandler);
-        signal(SIGTERM, sigHandler);
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGINT, sigHandler);
+    signal(SIGTERM, sigHandler);
 
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-        check(getaddrinfo(NULL, "8080", &hints, &servinfo) == 0,
-                        "cannot getaddrinfo");
+    check(getaddrinfo(NULL, "8080", &hints, &servinfo) == 0,
+          "cannot getaddrinfo");
 
-        sfd =
-                socket(servinfo->ai_family, servinfo->ai_socktype,
-                                servinfo->ai_protocol);
+    sfd =
+        socket(servinfo->ai_family, servinfo->ai_socktype,
+               servinfo->ai_protocol);
 
-        optval = 1;
-        setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    optval = 1;
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-        check(bind(sfd, servinfo->ai_addr, servinfo->ai_addrlen) != -1,
-                        "cannot bind.");
+    check(bind(sfd, servinfo->ai_addr, servinfo->ai_addrlen) != -1,
+          "cannot bind.");
 
-        check(listen(sfd, 10) != -1, "Cannot listen");
+    check(listen(sfd, 100) != -1, "Cannot listen");
 
-        while (1) {
-                check((newfd = accept(sfd, NULL, NULL)) != -1, "cannot accept");
+    while (1) {
+        check((newfd = accept(sfd, NULL, NULL)) != -1, "cannot accept");
 
-                switch (fork()) {
-                        case 0:
-                                //printf("fork");
-                                proxy(newfd);
-                                break;
-                        default:
-                                close(newfd);
-                                continue;
-                }
-                // printf("finish");
+        switch (fork()) {
+        case 0:
+            // printf("fork");
+            proxy(newfd);
+            break;
+        default:
+            close(newfd);
+            continue;
         }
+        // printf("finish");
+    }
 
-        return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 
-error:
-        return EXIT_FAILURE;
+  error:
+    return EXIT_FAILURE;
 }
